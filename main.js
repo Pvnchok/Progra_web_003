ea0ac94314120d4d03a798ffb15d06b0aef56dcf
Pvnchok <fr.molinas@duocuc.cl>
2024-05-22 13:11:48 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,68 @@
+
+
+// Cuando el DOM esté listo
+
+$(document).ready(function() {
+
+ // Validar el formulario
+
+ $("#miFormulario").validate({
+
+  errorClass: "error",
+
+  validClass: "valid",
+
+  rules: {
+
+   // Reglas para el campo correo electrónico
+
+   email: {
+
+    required: true,
+
+    email: true
+
+   },
+
+   // Reglas para el campo contraseña
+
+   password: {
+
+    required: true,
+
+    minlength: 10
+
+   }
+
+  },
+
+  messages: {
+
+   email: "Por favor, ingresa un correo electrónico válido",
+
+   password: {
+
+    required: "Por favor, crea una contraseña",
+
+    minlength: "Tu contraseña debe tener al menos 10 caracteres"
+
+   }
+
+  },
+
+  highlight: function(element) {
+
+   $(element).addClass('error').removeClass('valid');
+
+  },
+
+  unhighlight: function(element) {
+
+   $(element).removeClass('error').addClass('valid');
+
+  }
+
+ });
+
+});
+
